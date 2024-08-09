1c1ff399a15c6eee56bbe1b636b7a41d14649da4
laimontr <128290019+TRSandwich@users.noreply.github.com>
2024-08-09 22:50:36 +0100

    Added Project and Code

---
+++ HelloWorldVB/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+	Sub Main(args As String())
+		Console.WriteLine("Hello World")
+		Console.Write("Press any key to continue")
+		Console.ReadKey(True)
+	End Sub
+End Module
